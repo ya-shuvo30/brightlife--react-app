bb59fd4e97e536a77c2636b311b224460408b89f
Yeasin Arafat <123yeasinarafat@gmail.com>
2025-08-20 13:26:04 +0600

    Fix base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/brightlife--react-app/',
+  base: '/', // <-- This is the corrected line for a custom domain
 })
